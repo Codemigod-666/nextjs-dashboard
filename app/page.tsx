2245b2a6f3453067701ab31f87475c836163d294
Codemigod-666 <vikay434@gmail.com>
2023-12-05 16:06:31 +0530

    commiting the changes

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return (
+  return ( 
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
